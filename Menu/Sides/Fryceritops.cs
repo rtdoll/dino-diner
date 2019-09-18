7bc93d80a3ef291e79d1043fb88d59860d942c4a
Ryan Doll <rtdoll@users.campus>
2019-09-18 15:22:59 -0500

    added some side stuff

---
+++ Menu/Sides/Fryceritops.cs
@@ -0,0 +1,36 @@
+﻿using DinoDiner.Menu.Sides;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Menu.Sides
+{
+    public class Fryceritops : Side
+    {
+        public Fryceritops()
+        {
+            this.Price = 0.99;
+            this.Calories = 222;
+            this.Ingredients = new List<string>() { "Potato", "Salt", "Vegetable Oil" };
+        }
+
+        public override Size Size
+        {
+            get
+            {
+                return this.Size;
+            }
+            set
+            {
+                Size = value;
+                switch (Size)
+                {
+                    case Size.Large:
+                        Price = 1.95;
+                        Calories = 420;
+                        break;
+                }
+            }
+        }
+    }
+}
